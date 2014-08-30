37a30e54c3f919221e696ce846b697f387eafde1
deepak <dpak.cherian@gmail.com>
2014-08-30 17:24:35 -0400

    updated dcline to use cell arrays of strings as labels

--- dc/plots/dcline.m
+++ dc/plots/dcline.m
@@ -1,6 +1,6 @@
 % Draws horizontal or vertical line and adds tick mark
 % called by linex and liney
-function [handles] = dcline(ax,x,label,color)
+function [handles, txthandles] = dcline(ax,x,label,color)
 
     hFig = evalin('caller','gcf');
     hAxis = evalin('caller','gca');
@@ -17,6 +17,10 @@ function [handles] = dcline(ax,x,label,color)
         label = repmat(label,[length(x) 1]);
     end
     
+    if ~iscell(label) && ~isempty(label)
+        label = cellstr(label);
+    end
+    
     if ax == 'x'
         yax  = get(hAxis,'YLim');
         tickstr = 'XTick';
@@ -28,19 +32,20 @@ function [handles] = dcline(ax,x,label,color)
     tick = get(hAxis,tickstr);
     
     handles = nan(size(x));
+    txthandles = nan(size(x));
     
     for i=1:length(x)
         if ax == 'x'
             handles(i) = plot([x(i) x(i)],yax,'--','LineWidth',2,'Color',color);
             if ~isempty(label)
-                text(double(x(i)),double(yax(end)-(length(label))/2),label(i,:), ...
-                    'Rotation',0,'VerticalAlignment','Bottom','FontSize',10,'Color',color);%,'FontWeight','Bold');
+                txthandles(i) = text(double(x(i)),double(yax(end)-(length(label{i}))/2),label{i}, ...
+                    'Rotation',0,'VerticalAlignment','Bottom','FontSize',16,'Color',color);%,'FontWeight','Bold');
             end
         else
             handles(i) = plot(xax,[x(i) x(i)],'--','LineWidth',2,'Color',color);
             if ~isempty(label)
-                text(double(xax(end)-(length(label)))/2,double(x(i)),label(i,:), ...
-                    'Rotation',0,'VerticalAlignment','Bottom','FontSize',10,'Color',color);%,'FontWeight','Bold');
+                txthandles(i) = text(double(xax(end)-(length(label{i})))/2,double(x(i)),label{i}, ...
+                    'Rotation',0,'VerticalAlignment','Bottom','FontSize',16,'Color',color);%,'FontWeight','Bold');
             end
         end
     end
