b74eb2d65f5b9f71c6d02f5c94f06bd7a43cfbd9
deepak <dpak.cherian@gmail.com>
2015-01-26 15:52:51 -0500

    diverging colorbar when using center_colorbar

--- dc/plots/center_colorbar.m
+++ dc/plots/center_colorbar.m
@@ -5,4 +5,7 @@ function [] = center_colorbar()
     
     a = max(abs(clim));
     caxis([-a a]);
-    
+
+    % if this is called, then I want a diverging colorbar with
+    % white at 0.
+    colormap(flipud(cbrewer('div','RdBu', 32)));
