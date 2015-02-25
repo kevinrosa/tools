989744c06ff0fd68ab7160173bebdb4af8804041
deepak <dpak.cherian@gmail.com>
2015-02-25 11:11:05 -0500

    minor updates to dc_roms_read_data

--- dc/ROMS/dc_roms_read_data.m
+++ dc/ROMS/dc_roms_read_data.m
@@ -3,14 +3,14 @@
 
 function [out,xax,yax,zax,grd] = dc_roms_read_data(folder,varname,tindices, ...
                         volume,stride,grd, ftype, dtype)
-    
+
     disp(['Reading ' varname]);
-    
+
     % set flags
     k = 1;
     quitflag = 0;
     objflag = 0;
-    
+
     % set inputs
     if ~exist('tindices','var') || isempty(tindices), tindices = [1 Inf]; end
     if ~exist('volume','var') || isempty(volume), volume = {}; end
@@ -18,15 +18,15 @@ function [out,xax,yax,zax,grd] = dc_roms_read_data(folder,varname,tindices, ...
     if ~exist('grd','var'), grd = []; end
     if ~exist('ftype', 'var') || isempty(ftype), ftype = 'avg'; end
     if ~exist('dtype', 'var') || isempty(dtype), dtype = 'double'; end
-    
+
     if length(tindices) == 1, tindices(2) = tindices(1); end
-    
+
     if isobject(folder)
         run = folder;
         folder = run.dir;
         objflag = 1;
     end
-    
+
     tic;
     if strcmpi(varname,'vor')
         files{1}='ocean_vor.nc';
@@ -35,18 +35,18 @@ function [out,xax,yax,zax,grd] = dc_roms_read_data(folder,varname,tindices, ...
         if isdir(folder)
             files = roms_find_file(folder, ftype);
         else
-            files = folder;
+            files = {folder};
         end
     end
-    
+
     %unused params
     slab = Inf;
-    
+
     for ii=1:length(files)
         if isdir(folder)
             fname = [folder '/' char(files(ii))];
         else
-            fname = folder;
+            fname = char(folder);
         end
 
         % variable information
@@ -96,10 +96,11 @@ function [out,xax,yax,zax,grd] = dc_roms_read_data(folder,varname,tindices, ...
 
         % process tindices (input) according to file
         [~,tnew,dt,~,tstride] = roms_tindices(tindices,slab,nt);
+
         % if tindices(2) is Inf and there are multiple files the case 2
         % does not execute since tnew is set by roms_tindices to be nt -
         % which comes from 1 file only.
-        if isinf(tindices(2)), tnew(2) = Inf; end
+        if isinf(tindices(end)), tnew(2) = Inf; end
         stride(end) = tstride(end);
 
         % Case 1 : if requested data not in this file, skip to next
@@ -117,7 +118,7 @@ function [out,xax,yax,zax,grd] = dc_roms_read_data(folder,varname,tindices, ...
                 tnew(2) = nt;
                 % set next read to start from beginning of new file
                 tindices(1) = 1;
-                tindices(2) = tindices(2)-nt;
+                tindices(end) = tindices(end)-nt;
             else
                 % Case 3 : requested data finishes in current file
                 if tnew(2) <= nt && ~isinf(tindices(end))
@@ -139,19 +140,19 @@ function [out,xax,yax,zax,grd] = dc_roms_read_data(folder,varname,tindices, ...
             out = temp;
             return;
         end
-        
+
         % make sure appending timestep always works
         try
             dimsave = max(ndims(temp),ndims(out));
         catch ME
             dimsave = ndims(temp);
         end
-        
+
         if isvector(temp)
             out(k:k+length(temp)-1) = temp;
             k = k+length(temp);
-        else        
-            % call ndims instead of using ndim because i could be 
+        else
+            % call ndims instead of using ndim because i could be
             % extracting a slice
             switch dimsave
                 case 2
@@ -160,7 +161,7 @@ function [out,xax,yax,zax,grd] = dc_roms_read_data(folder,varname,tindices, ...
                     out(:,:,k:k+size(temp,3)-1) = temp;
                 case 4
                     out(:,:,:,k:k+size(temp,4)-1) = temp;
-            end  
+            end
             if count(end) ~= 1
                 k = k + size(temp,dimsave);
             else
@@ -169,5 +170,5 @@ function [out,xax,yax,zax,grd] = dc_roms_read_data(folder,varname,tindices, ...
             clear temp;
         end
         if quitflag, break; end
-    end 
+    end
     toc;
