131190bd83aec3c329f0435538c7d92df224d89f
deepak <dpak.cherian@gmail.com>
2015-05-22 14:55:32 -0400

    use common preamble.tex for beamer and memoir. Clean up old files.
    Need to add an if condition for beamer, to remove errors from titletoc, titlesec

--- latex/preamble.bat
+++ latex/preamble.bat
@@ -1,2 +1,3 @@
 ################### xelatex -ini -jobname="preamble" "&xelatex" mylatexformat.ltx """preamble.tex"""
-xelatex -ini -jobname="preamble" "&xelatex preamble.tex\dump"
+xelatex -ini -jobname="preamble-memoir" "&xelatex preamble-memoir.tex\dump"
+xelatex -ini -jobname="preamble-beamer" "&xelatex preamble-beamer.tex\dump"
