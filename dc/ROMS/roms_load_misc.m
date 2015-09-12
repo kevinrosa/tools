959b7a50f612665031e77a4dbc0d0d713da55120
deepak <dpak.cherian@gmail.com>
2015-09-12 12:54:40 -0400

    Updated misc info loading.

--- dc/ROMS/roms_load_misc.m
+++ dc/ROMS/roms_load_misc.m
@@ -11,7 +11,8 @@ function [misc] = roms_load_misc(fname)
 
     if ~exist('fname','var'), fname = 'ocean_his.nc'; end
     
-    vars = {'nl_tnu2','nl_visc2','rdrg','rdrg2','rho0','R0','Tcoef','Scoef','h','f'};
+    vars = {'nl_tnu4', 'nl_visc4', 'nl_tnu2','nl_visc2','rdrg','rdrg2', ...
+            'rho0','R0','Tcoef','Scoef','h','f'};
     
     for i=1:length(vars)
         try
