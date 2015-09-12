ceeeb8023b68bf8aa4f3c03fdfac966540a0d0bf
deepak <dpak.cherian@gmail.com>
2015-09-12 13:30:07 -0400

    githash supports --git-dir now

--- dc/random/githash.m
+++ dc/random/githash.m
@@ -1,10 +1,16 @@
-function [hash] = githash(fname)
+function [hash] = githash(fname, gitdir)
 
     if ~exist('fname', 'var')
         fname = [];
     end
 
-    [~, hash] = system(['TERM=xterm-256color git log -n 1 ' ...
-                        '--pretty=format:''%H'' ' fname ';']);
+    if ~exist('gitdir', 'var')
+        gitdir = '';
+    else
+        gitdir = ['--git-dir=' gitdir];
+    end
+
+    [~, hash] = system(['TERM=xterm-256color git ' gitdir ...
+                        ' log -n 1 --pretty=format:''%H'' ' fname ';']);
     % remove bash escape characters
     hash = hash(9:48)
