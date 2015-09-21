88df3acaa87296510dad46f79d3710eabd3d3cf7
deepak <dpak.cherian@gmail.com>
2015-09-21 13:16:21 -0400

    linkfig supports color axis limits too

--- dc/plots/linkfig.m
+++ dc/plots/linkfig.m
@@ -5,5 +5,15 @@ function [] = linkfig(hfigs, axstr)
         ax(ii) = gca;
     end
 
+    % do colorbars too.
+    if findstr(axstr, 'c')
+        axstr(axstr == 'c') = [];
+        clim = [min(min(ax.CLim)) ...
+                max(max(ax.CLim))];
+        for ii=1:length(ax)
+            ax(ii).CLim = clim;
+        end
+    end
+
     linkaxes(ax, axstr);
 end
