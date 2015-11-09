1c3e307464277d1984bca1674baa278917fda314
deepak <dpak.cherian@gmail.com>
2015-11-09 10:49:38 -0500

    One more fillnan bugfix.

--- dc/random/fillnan.m
+++ dc/random/fillnan.m
@@ -1,14 +1,13 @@
 % Replaces val with NaN's in 'in'
-%       [out] = fillnan(in,val)
+%       [in] = fillnan(in,val)
 
-function [out] = fillnan(in,val,recursive)
+function [in] = fillnan(in,val,recursive)
     if islogical(in)
         % logicals cant have NaN
-        out = in;
         return;
     end
 
-    out(in == val) = NaN;
+    in(in == val) = NaN;
 
     if nargin == 2
         recursive = 0;
@@ -17,6 +16,6 @@ function [out] = fillnan(in,val,recursive)
     if val == Inf || val == -Inf
         if recursive == 1, return; end
         fprintf('\n Running fillnan recursively for +-Inf \n');
-        out = fillnan(out,-1*val,1);
+        in = fillnan(in,-1*val,1);
     end
         
