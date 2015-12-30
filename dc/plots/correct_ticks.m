0825457a8e84c0e0c3db6974e0263bcddc550fcf
deepak <dpak.cherian@gmail.com>
2015-12-30 11:40:23 -0500

    correct_ticks doesn't remove tick anymore.
    Instead, removes labels.

--- dc/plots/correct_ticks.m
+++ dc/plots/correct_ticks.m
@@ -9,26 +9,12 @@ function [] = correct_ticks(ax, format, ind, hax)
     if ~exist('ind', 'var'), ind = []; end
     if ~exist('hax', 'var'), hax = gca; end
 
+    limstr = [upper(ax) 'Lim'];
     tickstr = [upper(ax) 'Tick'];
     labelstr = [tickstr 'Label'];
 
     for aa = 1:length(hax)
         ticks = get(hax(aa), tickstr);
-        if ~isempty(ind)
-            if ~ischar(ind) && ~iscell(ind) % provided with index
-                ind(ind > length(ticks)) = [];
-                ticks(ind) = [];
-            else
-                if ~iscell(ind)
-                    ind = cellstr(ind);
-                end
-                for kk=1:length(ind)
-                    ticks(ticks == str2double(ind{kk})) = [];
-                end
-            end
-            set(hax(aa), tickstr, ticks);
-        end
-
         if ~isempty(format)
             for ii = 1:length(ticks)
                 ticklabels{ii} = sprintf(format, ticks(ii));
@@ -37,6 +23,34 @@ function [] = correct_ticks(ax, format, ind, hax)
                 end
             end
             set(hax(aa), labelstr, ticklabels);
+        else
+            ticklabels = hax.(labelstr);
         end
+
+        if length(ticks) ~= length(ticklabels)
+            ticks(ticks < hax(aa).(limstr)(1) ...
+                  | ticks > hax(aa).(limstr)(2)) = [];
+            hax(aa).(tickstr) = ticks;
+        end
+
+        if ~isempty(ind)
+            if ~ischar(ind) && ~iscell(ind) % provided with index
+                ind(ind > length(ticks)) = [];
+                ticklabels{ind} = '';
+            else
+                if ~iscell(ind)
+                    ind = cellstr(ind);
+                end
+                for kk=1:length(ind)
+                    for tt=1:length(ticklabels)
+                        if strcmpi(ticklabels{tt}, ind{kk})
+                            ticklabels{tt} = '';
+                        end
+                    end
+                end
+            end
+        end
+
+        set(hax(aa), labelstr, ticklabels);
     end
 end
