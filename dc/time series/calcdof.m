d43212ad9d114e1e160cc9e7a39722e607d1c55c
deepak <dpak.cherian@gmail.com>
2016-01-14 15:15:11 -0500

    Fix really bad bug in dof calculation.
    I was using the maximum value of area, not the time at which this max
    value occurs.

--- dc/time series/calcdof.m
+++ dc/time series/calcdof.m
@@ -4,16 +4,11 @@ function [dof, IntegralTimeScale] = calcdof(in)
 
     [c,lags] = xcorr(in - mean(in), 'coef');
     [cmax,imax] = max(c);
-    i0 = imax + find(c(imax:end) < 0, 1, 'first') - 1; % first zero crossing
 
     % calculate a bunch of timescales and take maximum
     % From Talley et al., Descriptive Physical Oceanography.
-    for tt=imax+3:length(c)
-        ff = tt - (imax+3) + 1;
-        CorrArea = trapz(c(imax:tt));
-        IntegralTimeScale(ff) = CorrArea/cmax;
-    end
+    CorrArea = cumtrapz(c(imax:end));
 
-    IntegralTimeScale = max(IntegralTimeScale);
+    [~,IntegralTimeScale] = max(CorrArea./cmax);
     dof = floor(length(in)/IntegralTimeScale);
 end
