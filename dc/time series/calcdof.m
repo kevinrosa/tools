4b5b9d5a3dbb65679abb5e5179d710a1d33cfee6
deepak <dpak.cherian@gmail.com>
2016-01-20 16:45:10 -0500

    Revert "Fix really bad bug in dof calculation."
    This reverts commit d43212ad9d114e1e160cc9e7a39722e607d1c55c.

--- dc/time series/calcdof.m
+++ dc/time series/calcdof.m
@@ -4,11 +4,16 @@ function [dof, IntegralTimeScale] = calcdof(in)
 
     [c,lags] = xcorr(in - mean(in), 'coef');
     [cmax,imax] = max(c);
+    i0 = imax + find(c(imax:end) < 0, 1, 'first') - 1; % first zero crossing
 
     % calculate a bunch of timescales and take maximum
     % From Talley et al., Descriptive Physical Oceanography.
-    CorrArea = cumtrapz(c(imax:end));
+    for tt=imax+3:length(c)
+        ff = tt - (imax+3) + 1;
+        CorrArea = trapz(c(imax:tt));
+        IntegralTimeScale(ff) = CorrArea/cmax;
+    end
 
-    [~,IntegralTimeScale] = max(CorrArea./cmax);
+    IntegralTimeScale = max(IntegralTimeScale);
     dof = floor(length(in)/IntegralTimeScale);
 end
