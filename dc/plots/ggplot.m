15ebb10be7b9ca53dac9fd1dd28a43f6c1f2ed2c
deepak <dpak.cherian@gmail.com>
2016-01-29 14:36:24 -0500

    Better ggplot.

--- dc/plots/ggplot.m
+++ dc/plots/ggplot.m
@@ -1,11 +1,14 @@
 function [] = ggplot()
 
     hax = gca;
-    axes(hax);
+
     grid on;
-    hax.Color = [1 1 1]*0.93;
+    hax.Color = [1 1 1]*0.90;
     hax.GridColor = [1 1 1];
     hax.GridAlpha = 0.65;
     hax.TickLength = [0 0];
-    hax.LineWidth = 1;
+    hax.LineWidth = 2;
+
+    hax.XRuler.Axle.ColorData = uint8([0 0 0 0])';
+    hax.YRuler.Axle.ColorData = uint8([0 0 0 0])';
 end
