fb887ed117071fb09467da6fc4c4e4b833cd38f1
deepak <dpak.cherian@gmail.com>
2016-04-25 19:35:01 -0400

    Updated tanh_fit.
    Much improved, returns confidence bouds.

--- dc/tanh_fit.m
+++ dc/tanh_fit.m
@@ -1,5 +1,6 @@
-% I want to fit y = y_0 tanh(x/X)
-function [y0, X] = tanh_fit(x, y, plot_flag, test)
+% I want to fit y = y_0 tanh((x-x0)/X) + y_1
+%  [y0, X, x0, y1, conf, fitobj] = tanh_fit(x, y, plot_flag, test)
+function [y0, X, x0, y1, conf, fitobj] = tanh_fit(x, y, plot_flag, test)
 
     if ~exist('test', 'var'), test = 0; end
     if ~exist('plot_flag', 'var'), plot_flag = 0; end
@@ -9,39 +10,68 @@ function [y0, X] = tanh_fit(x, y, plot_flag, test)
         return;
     end
 
+    if size(x,1) ~= 1, x = x'; end
+    if size(y,1) ~= 1, y = y'; end
+
+    ym = mean(y);
+    warning('subtracting mean for better fit');
+    y = y - ym;
+
     initGuess(1) = max(y(:));
     initGuess(2) = max(x(:))/2;
+    initGuess(3) = 0;
+    initGuess(4) = 0;
 
     opts = optimset('MaxFunEvals',1e7);
-    [fit2,~,exitflag] = fminsearch(@(fit) fiterror(fit,x,y), ...
+    [fit2,~,exitflag] = fminsearch(@(f) fiterror(f,x,y), ...
                                    initGuess,opts);
 
     y0 = fit2(1);
     X = fit2(2);
+    x0 = fit2(3);
+    y1 = fit2(4) + ym;
+
+    tanhfn = fittype(@(y0,X,x0,y1,x) y0*tanh((x-x0)/X) + y1);
+    fitobj = fit(x', y', tanhfn, 'StartPoint', fit2);
+    conf = confint(fitobj);
+
+    y0 = fitobj.y0;
+    X = fitobj.X;
+    if abs(conf(1,2)) > abs(conf(2,2))
+        temp = conf(2,2);
+        conf(2,2) = conf(1,2);
+        conf(1,2) = temp;
+    end
+    x0 = fitobj.x0;
+    y1 = fitobj.y1 + ym;
 
     if plot_flag
-        figure;
-        plot(x,y,'k*'); hold all
-        plot(x,y0*tanh(x/X));
-        linex([1 2 3]*X);
+        figure; hold on;
+        plot(x,y,'k.', 'MarkerSize', 12);
+        plot(fitobj, 'predobs');
+        plot(fitobj,x',y', 'residuals')
+        %        linex([1 2 3]*X);
     end
 end
 
-function [E] = fiterror(fit,x,y)
-    % x = (T0,H,a)
-    y0 = fit(1); X = fit(2);
+function [E] = fiterror(f,x,y)
+    y0 = f(1); X = f(2); x0 = f(3); y1 = f(4);
 
-    E = sum((y - y0 .* tanh(x/X)).^2);
+    E = sum((y - y0 .* tanh((x-x0)/X) - y1).^2);
 end
 
 function [] = test_fit()
     x = [0.02:0.05:4];
     X = 2;
     y0 = 2;
-    y = y0 * tanh(x/X) + rand(size(x)).*y0/4;
+    x0 = 0;
+    y1 = 2;
+    y = y0 * tanh((x-x0)/X) + y1; % + rand(size(x)).*y0/4;
 
-    [yy,xx] = tanh_fit(x,y,1);
+    [Y0, XX, X0, Y1] = tanh_fit(x,y,1);
 
-    disp(['y0 = ' num2str(yy) ' | Original = ' num2str(y0)]);
-    disp(['X = ' num2str(xx) ' | Original = ' num2str(X)]);
+    disp(['y0 = ' num2str(Y0) ' | Original = ' num2str(y0)]);
+    disp(['X = ' num2str(XX) ' | Original = ' num2str(X)]);
+    disp(['x0 = ' num2str(X0) ' | Original = ' num2str(x0)]);
+    disp(['y1 = ' num2str(Y1) ' | Original = ' num2str(y1)]);
 end
